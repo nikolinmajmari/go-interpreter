91f291ae19ed07ffaffde6bed81dc7418ce136ed
Nikolin Majmari <nikolinmajmari30@gmail.com>
2024-09-07 11:16:20 +0200

    added support for parsing integer expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,9 +26,59 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
-
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(left, node.Operator, right)
 	}
 	return nil
+}
+
+func evalInfixExpression(left object.Object, operator string, right object.Object) object.Object {
+	l, rok := left.(*object.Integer)
+	if !rok {
+		return nil
+	}
+	r, lok := right.(*object.Integer)
+	if !lok {
+		return nil
+	}
+	switch operator {
+	case "+":
+		return evalPlusOperatorInfixExpression(l, r)
+	case "-":
+		return evalMinusOperatorInfixExpression(l, r)
+	case "*":
+		return evalMultiplyOperatorInfixExpression(l, r)
+	case "/":
+		return evalDivisionOperatorInfixExpression(l, r)
+	default:
+		return nil
+	}
+}
+
+func evalDivisionOperatorInfixExpression(l *object.Integer, r *object.Integer) *object.Integer {
+	return &object.Integer{
+		Value: l.Value / r.Value,
+	}
+}
+
+func evalMultiplyOperatorInfixExpression(l *object.Integer, r *object.Integer) *object.Integer {
+	return &object.Integer{
+		Value: l.Value * r.Value,
+	}
+}
+
+func evalMinusOperatorInfixExpression(l *object.Integer, r *object.Integer) *object.Integer {
+	return &object.Integer{
+		Value: l.Value - r.Value,
+	}
+}
+
+func evalPlusOperatorInfixExpression(left *object.Integer, right *object.Integer) *object.Integer {
+	return &object.Integer{
+		Value: left.Value + right.Value,
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
@@ -37,7 +87,6 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 		return evalBangOperatorExpression(right)
 	case "-":
 		return evalMinusOperatorExpression(right)
-
 	}
 	return nil
 }
